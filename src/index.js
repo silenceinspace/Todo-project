de170607011ec287de83943e8108f6b8b1d9366d
Anton <anton20020426@gmail.com>
2023-07-14 19:52:17 +0200

    Indicate a chosen project + add a local add button

--- src/index.js
+++ src/index.js
@@ -6,84 +6,127 @@ import "./styles.css";
 
 // Create the main layout
 const content = document.querySelector("#content");
-const defaultBlock = createElement("div", "default-div", "");
+const inboxBlock = createElement("div", "inbox-div", "");
 const projectBlock = createElement("div", "project-div", "");
 const todoBlock = createElement("div", "todo-div", "");
 
-appendElement(content, defaultBlock);
+appendElement(content, inboxBlock);
 appendElement(content, projectBlock);
 appendElement(content, todoBlock);
 
 // Default block
-const inboxBtn = createElement("button", "btn-default", "inbox");
-const today = createElement("button", "btn-today", "today");
-const upcoming = createElement("button", "btn-upcoming", "upcoming");
+const inbox = createElement("button", "button-inbox", "inbox");
+inbox.disabled = true;
+inbox.classList.add("project");
+const today = createElement("button", "button-today", "today");
+const upcoming = createElement("button", "button-upcoming", "upcoming");
 
-appendElement(defaultBlock, inboxBtn);
-appendElement(defaultBlock, today);
-appendElement(defaultBlock, upcoming);
+appendElement(inboxBlock, inbox);
+appendElement(inboxBlock, today);
+appendElement(inboxBlock, upcoming);
 
 // For now to allow task/project creation
-const createTasksBtn = createElement("button", "create-tasks", "Create task");
-const createProjectsBtn = createElement(
+const createTaskButton = createElement("button", "create-tasks", "Create task");
+const createProjectButton = createElement(
   "button",
   "create-projects",
   "Create project"
 );
 const displayForTasks = createElement("div", "display-tasks", "");
 
-appendElement(todoBlock, createTasksBtn);
-appendElement(todoBlock, createProjectsBtn);
+appendElement(todoBlock, createTaskButton);
+appendElement(todoBlock, createProjectButton);
 appendElement(todoBlock, displayForTasks);
 
 // Main array where tasks are stored
 let mainStorage = [];
 console.log("Main storage:");
 console.log(mainStorage);
+// Store projects
+let projectStorage = [];
+console.log("Project storage:");
+console.log(projectStorage);
 
-// Create a todo
-createTasksBtn.addEventListener("click", createTodo);
-function createTodo() {
-  let project = prompt("Project?", "");
-  // Default project - if projectStorage.length == 0 ---> means that no custom projects yet
-  if (project === "") {
-    project = "inbox";
+// Create a todo directly in a task
+const createTaskInsideProject = createElement(
+  "button",
+  "create-task-directly",
+  "+"
+);
+appendElement(todoBlock, createTaskInsideProject);
+createTaskInsideProject.addEventListener("click", createTodo);
+
+// Create a todo globally - project name is required!!!
+createTaskButton.addEventListener("click", createTodo);
+function createTodo(e) {
+  // Create a task locally inside the chosen task
+  const directCreation = e.target.closest(".create-task-directly");
+  let project;
+
+  if (directCreation) {
+    const currentlyChosenProject =
+      document.querySelector("button[disabled]").textContent;
+    project = currentlyChosenProject;
+  } else {
+    project = prompt("Project?", "");
+    // Default project, there are no custom projects yet
+    if (projectStorage.length === 0) {
+      alert(
+        "You have not created any project yet. Your tasks are put into inbox"
+      );
+      project = "inbox";
+    }
   }
+
   let title = prompt("Title", "");
   let priority = prompt("Priority", "");
 
   const task = new Todo(project, title, priority);
   mainStorage = taskInterface.add(task);
   console.log(mainStorage);
+
+  updateTaskDisplay();
 }
 
 // Display all todos in a project
-inboxBtn.addEventListener("click", displayTasksInProject);
-projectBlock.addEventListener("click", displayTasksInProject);
+inbox.addEventListener("click", displayThisProjectTasks);
+projectBlock.addEventListener("click", displayThisProjectTasks);
 
-function displayTasksInProject(e) {
+function displayThisProjectTasks(e) {
   // Click occurs on the inside of a button
   const btn = e.target.closest("button");
   if (!btn) return;
 
+  let disabledButtons = document.querySelectorAll("button.project");
+  disabledButtons.forEach((btn) => (btn.disabled = false));
+
+  btn.disabled = true;
+
   const projectName = e.target.textContent;
-  console.log(projectName);
+  generateListOfTasks(projectName);
+}
 
-  const currentProject = taskInterface.findAll(projectName);
+function updateTaskDisplay() {
+  const projectName = document.querySelector("button[disabled]").textContent;
 
+  generateListOfTasks(projectName);
+}
+
+function generateListOfTasks(project) {
+  const currentProject = taskInterface.findAll(project);
   // Clear the todo block not to duplicate appended elements
   displayForTasks.replaceChildren();
 
   for (let i = 0; i < currentProject.length; i++) {
-    const taskField = createElement("div", "task-field", "");
-    taskField.setAttribute("data-id", currentProject[i].id);
-    const removeTask = createElement("button", "rmv-task", "X");
+    const taskDiv = createElement("div", "task-div", "");
+    taskDiv.setAttribute("data-id", currentProject[i].id);
+    const removeButton = createElement("button", "remove-task", "X");
     const titlePara = currentProject[i].todoTitle;
     const projectPara = currentProject[i].todoProject;
     const priorityPara = currentProject[i].todoPriority;
 
-    appendElement(displayForTasks, taskField);
-    appendElement(taskField, removeTask);
+    appendElement(displayForTasks, taskDiv);
+    appendElement(taskDiv, removeButton);
 
     for (let j = 0; j < 3; j++) {
       let para;
@@ -101,7 +144,7 @@ function displayTasksInProject(e) {
             `Priority: ${priorityPara}`
           );
       }
-      appendElement(taskField, para);
+      appendElement(taskDiv, para);
     }
   }
 }
@@ -122,12 +165,7 @@ function removeTask(e) {
 }
 
 // Create a project
-// Store projects
-let projectStorage = [];
-console.log("Project storage:");
-console.log(projectStorage);
-
-createProjectsBtn.addEventListener("click", createProject);
+createProjectButton.addEventListener("click", createProject);
 function createProject() {
   const title = prompt("Project name?", "");
   if (title === "" || !title) return;
@@ -137,7 +175,8 @@ function createProject() {
 
   const div = createElement("div", "project-title-div", "");
   const btn = createElement("button", "custom-project-div", title);
-  const span = createElement("span", "rmv-project", "X");
+  btn.classList.add("project");
+  const span = createElement("span", "remove-project", "X");
 
   appendElement(div, btn);
   appendElement(div, span);
@@ -145,9 +184,7 @@ function createProject() {
 }
 
 // Remove a project
-
 projectBlock.addEventListener("click", removeProject);
-
 function removeProject(e) {
   const span = e.target.closest("span");
   if (!span) return;
@@ -165,5 +202,8 @@ function removeProject(e) {
     projectStorage = projectInterface.remove(project);
     span.parentNode.remove();
     displayForTasks.replaceChildren();
+
+    inbox.disabled = true;
+    updateTaskDisplay();
   }
 }
